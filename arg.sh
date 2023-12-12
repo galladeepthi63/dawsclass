789c363a0de94ecb055ab3db8ed16100b4b4ae4a
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-12 18:10:22 +0530

    entryed the username and pwd

--- arg.sh
+++ arg.sh
@@ -2,5 +2,5 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1: hello $PERSON1,how are you"
+echo "$PERSON1: hello $PERSON2,how are you"
 echo "$PERSON2: hi $PERSON1,i am good"
---
+++ entrypwd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Please enter username"
+read -s USERNAME
+echo "please enter password"
+read -s PASSWORD
+echo "username is:$USERNAME,password is:$PASSWORD"
