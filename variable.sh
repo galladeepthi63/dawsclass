59205af2285931bfa5b8b4f185bcc31d74f955d3
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-12 17:56:03 +0530

    hello word file

--- variable.sh
+++ variable.sh
@@ -2,5 +2,5 @@
 PERSON1=divya
 PERSON2=aruna
 
-echo "$PERSON1: hello $PERSON1,how are you
-echo "$PERSON2: hi $PERSON1,i am good
+echo "$PERSON1: hello $PERSON1,how are you"
+echo "$PERSON2: hi $PERSON1,i am good"
