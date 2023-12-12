79f21abe6be7fc95b1776052b16df30ac217e430
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-12 18:27:01 +0530

    upated array file

--- array.sh
+++ array.sh
@@ -1,7 +1,6 @@
 #!bin/bash
 FRUITS=("apple" "orange" "mango")
-echo "first vale: $[FRUITS[0]}"
-echo "second vale: $[FRUITS[1]}"
-echo "third vale: $[FRUITS[2]}"
-echo "all fruits: $[FRUITS[@]}"
-
+echo "first vale: ${[FRUITS[0]}"
+echo "second vale: ${[FRUITS[1]}"
+echo "third vale: ${[FRUITS[2]}"
+echo "all fruits: ${[FRUITS[@]}
