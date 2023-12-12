7f5b44966617ff7b48fa23b2c1677ff61482f62b
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-12 18:30:28 +0530

    array file

--- array.sh
+++ array.sh
@@ -3,4 +3,4 @@ FRUITS=("apple" "orange" "mango")
 echo "first vale: ${FRUITS[0]}"
 echo "second vale: ${FRUITS[1]}"
 echo "third vale: ${FRUITS[2]}"
-echo "all fruits: ${FRUITS[@]}
+echo "all fruits: ${FRUITS[@]}"
