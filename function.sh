47797f51cad322793b1d83cbcac689542808a6de
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-13 10:32:56 +0530

    function file

---
+++ function.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2 ... FAILED"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+yum install git -y
+
+VALIDATE $? "Installing GIT"
