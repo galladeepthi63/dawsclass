9c0943de90a60b14500d5e042e6337a748445e05
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-13 10:35:09 +0530

    function file

--- function.sh
+++ function.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -8,6 +9,14 @@ VALIDATE(){
         echo "$2 ... SUCCESS"
     fi
 }
+    if [ $ID -ne 0 ]
+    then
+        echo "ERROR:: Please run this script with root access"
+        exit 1 # you can give other than 0
+    else
+        echo "You are root user"
+    fi # fi means reverse of if, indicating condition end
+
 yum install git -y
 
 VALIDATE $? "Installing GIT"
