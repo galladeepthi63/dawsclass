cca5fbfa2a020cee59d014678a4f5c9c274fdab5
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-13 09:58:14 +0530

    condition file

---
+++ condition.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+NUMBER1= 10
+NUMBER2= 20
+
+if [$NUMBER1 -gt $NUMBER2]
+then
+    echo " Give number $NUMBER1 is greater than $NUMBER2"
+else
+    echo " Give number $NUMBER1 is not greater than $NUMBER2"
+fi
