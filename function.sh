e5460d7cbdac0d95db384958c33a8d11b6d5b251
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-13 10:36:54 +0530

    function file

--- function.sh
+++ function.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 ID=$(id -u)
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -9,13 +11,18 @@ VALIDATE(){
         echo "$2 ... SUCCESS"
     fi
 }
-    if [ $ID -ne 0 ]
-    then
-        echo "ERROR:: Please run this script with root access"
-        exit 1 # you can give other than 0
-    else
-        echo "You are root user"
-    fi # fi means reverse of if, indicating condition end
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+yum install mysql -y
+
+VALIDATE $? "Installing MySQL"
 
 yum install git -y
 
