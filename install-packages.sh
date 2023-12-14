de71249ce587a52570daf19dcfe4f05e9b030cd1
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-14 15:51:47 +0530

    install the packages

---
+++ install-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ $ID ne 0]
+then 
+    echo "ERROR:: Please run this script with root user "
+    exit 1
+else 
+    echo "you are root user "
+fi
